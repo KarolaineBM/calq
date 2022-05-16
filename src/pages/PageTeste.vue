372f2ee6d553a66075affe9d7348068e2dbe7e83
Karolaine Barros <karolainebarrosm@gmail.com>
2022-05-16 18:01:34 -0300

    metodos criados

--- src/pages/PageTeste.vue
+++ src/pages/PageTeste.vue
@@ -2,7 +2,7 @@
   <q-page class="row flex-center">
     <div class="col-md-5 col-xs-12 q-pa-md">
       <div class="row">
-        <q-input class="full-width" outlined> </q-input>
+        <q-input class="full-width" outlined v-model="display"></q-input>
       </div>
 
       <div class="row">
@@ -11,8 +11,9 @@
             class="full-width"
             size="lg"
             color="blue-8"
-            id="display"
-          ></q-btn>
+            @click="apagaTudo()"
+            >AC</q-btn
+          >
         </div>
 
         <div class="col-3">
@@ -35,15 +36,33 @@
 
       <div class="row">
         <div class="col-3">
-          <q-btn class="full-width" size="lg" color="blue-5" label="7"></q-btn>
+          <q-btn
+            class="full-width"
+            size="lg"
+            color="blue-5"
+            @click="calNumeros(7)"
+            >7</q-btn
+          >
         </div>
 
         <div class="col-3">
-          <q-btn class="full-width" size="lg" color="blue-5" label="8"></q-btn>
+          <q-btn
+            class="full-width"
+            size="lg"
+            color="blue-5"
+            @click="calNumeros(8)"
+            >8</q-btn
+          >
         </div>
 
         <div class="col-3">
-          <q-btn class="full-width" size="lg" color="blue-5" label="9"></q-btn>
+          <q-btn
+            class="full-width"
+            size="lg"
+            color="blue-5"
+            @click="calNumeros(9)"
+            >9</q-btn
+          >
         </div>
 
         <div class="col-3">
@@ -53,15 +72,33 @@
 
       <div class="row">
         <div class="col-3">
-          <q-btn class="full-width" size="lg" color="blue-5" label="6"></q-btn>
+          <q-btn
+            class="full-width"
+            size="lg"
+            color="blue-5"
+            @click="calNumeros(6)"
+            >6</q-btn
+          >
         </div>
 
         <div class="col-3">
-          <q-btn class="full-width" size="lg" color="blue-5" label="5"></q-btn>
+          <q-btn
+            class="full-width"
+            size="lg"
+            color="blue-5"
+            @click="calNumeros(5)"
+            >5</q-btn
+          >
         </div>
 
         <div class="col-3">
-          <q-btn class="full-width" size="lg" color="blue-5" label="4"></q-btn>
+          <q-btn
+            class="full-width"
+            size="lg"
+            color="blue-5"
+            @click="calNumeros(4)"
+            >4</q-btn
+          >
         </div>
 
         <div class="col-3">
@@ -71,29 +108,65 @@
 
       <div class="row">
         <div class="col-3">
-          <q-btn class="full-width" size="lg" color="blue-5" label="3"></q-btn>
+          <q-btn
+            class="full-width"
+            size="lg"
+            color="blue-5"
+            @click="calNumeros(3)"
+            >3</q-btn
+          >
         </div>
 
         <div class="col-3">
-          <q-btn class="full-width" size="lg" color="blue-5" label="2"></q-btn>
+          <q-btn
+            class="full-width"
+            size="lg"
+            color="blue-5"
+            @click="calNumeros(2)"
+            >2</q-btn
+          >
         </div>
 
         <div class="col-3">
-          <q-btn class="full-width" size="lg" color="blue-5" label="1"></q-btn>
+          <q-btn
+            class="full-width"
+            size="lg"
+            color="blue-5"
+            @click="calNumeros(1)"
+            >1</q-btn
+          >
         </div>
 
         <div class="col-3">
-          <q-btn class="full-width" size="lg" color="blue-8" label="+"></q-btn>
+          <q-btn
+            class="full-width"
+            size="lg"
+            color="blue-8"
+            @click="botaoDeMais()"
+            >+</q-btn
+          >
         </div>
       </div>
 
       <div class="row">
         <div class="col-3">
-          <q-btn class="full-width" size="lg" color="blue-5" label="0"></q-btn>
+          <q-btn
+            class="full-width"
+            size="lg"
+            color="blue-5"
+            @click="calNumeros(0)"
+            >0</q-btn
+          >
         </div>
 
         <div class="col-3">
-          <q-btn class="full-width" size="lg" color="blue-5" label="0"></q-btn>
+          <q-btn
+            class="full-width"
+            size="lg"
+            color="blue-5"
+            @click="calNumeros(0)"
+            >0</q-btn
+          >
         </div>
 
         <div class="col-3">
@@ -116,7 +189,29 @@ export default defineComponent({
   name: "PageTeste",
 
   setup() {
-    return {};
+    //variaveis
+    let display = ref("");
+    const total = ref("");
+
+    //metodos
+    const calNumeros = (numeros) => {
+      display.value = display.value + numeros;
+    };
+
+    const botaoDeMais = () => {
+      display.value = display.value;
+    };
+
+    const apagaTudo = () => {};
+
+    return {
+      ref,
+      //valorTotal,
+      display,
+      botaoDeMais,
+      calNumeros,
+      apagaTudo,
+    };
   },
 });
 </script>
